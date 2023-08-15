4dbb5a1007eade5ef8ea3466032c1c4a93608ecd
RodrigoSante <132940978+RodrigoSante@users.noreply.github.com>
2023-08-15 15:47:32 -0300

    Contact form ready.

--- src/contactUs/ContactUs.js
+++ src/contactUs/ContactUs.js
@@ -1,43 +1,13 @@
-/*import React, {useRef} from "react";
-import emailjs from "@emailjs/browser";
-import "../styles/StyleContact.css";
-
-export const ContactUs = () => {
-
-    const refForm = useRef();
-
-    const hundleSubmit = (event) => {
-        event.PreventDefault();
-        
-        const serviceId = "service_pqrpwnr";
-        const templateId = "template_xtt7ihh";
-        const apiKey = "3e0ECI0oMFS6P_F-5";
-
-        emailjs.sendForm(serviceId, templateId, refForm.current, apiKey)
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    }
-
-    return(
-        <form className="form" ref={refForm} action="" onSubmit={hundleSubmit}>
-            <div class="form-conteiner">
-                <h2 class="form-tittle">Contact Me</h2>
-                <input name="user_name" type="text" className="form-input" placeholder="Name:"></input>
-                <input type="email" name="user_email" className="form-input" placeholder="Email:"></input>
-                <textarea name="message" className="form-input-message" placeholder="Message:"></textarea>   
-                <input type="submit" value="Send" className="form-cta"/>
-              </div>
-        </form>
-    );
-}*/
-
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import emailjs from '@emailjs/browser';
+import "../styles/StyleContact.css";
+import {IntlProvider, FormattedMessage} from "react-intl";
+import { langContext } from "../context/langContext";
 
 export const ContactUs = () => {
+  
+  const idioma = useContext(langContext);
+  
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -52,14 +22,21 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+    <form ref={form} onSubmit={sendEmail} className="form">
+      <div class="form-conteiner">
+        <h2 class="form-tittle">
+          <FormattedMessage
+              id="app.contactme"
+              defaultMessage="Contact Me"/>
+        </h2>
+        <label className="label-contact"><FormattedMessage id="app.contactname" defaultMessage="Name"/></label>
+        <input type="text" name="user_name" className="form-input" />
+        <label className="label-contact"><FormattedMessage id="app.contactemail" defaultMessage="Email"/></label>
+        <input type="email" name="user_email" className="form-input" />
+        <label className="label-contact"><FormattedMessage id="app.contactmessage" defaultMessage="Message"/></label>
+        <textarea name="message" className="form-input-message" />
+        <button className="form-cta"><FormattedMessage id="app.sendmessage" defaultMessage="Send"/></button>
+      </div>
     </form>
   );
 };
